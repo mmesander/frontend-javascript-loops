d4c4df9b427bc9ea0d4c41452a44dd60da7a55fa
mmesander <mark_mesander@hotmail.com>
2023-03-27 17:36:42 +0200

    Opdracht 2 af

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -38,7 +38,15 @@ const numbers = [2, 4, 5, 29, 38];
 
 console.log("Opdracht 2:")
 console.log("-------------------------------")
-
+console.log(numbers)
+for (let i = 0; i <= names.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        numbers[i] = numbers[i] * 2
+    } else {
+        numbers[i] = numbers[i] * 3
+    }
+}
+console.log(numbers)
 console.log("-------------------------------")
 console.log(" ")
 
